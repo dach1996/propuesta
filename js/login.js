a57b31a9de4b9621e76cd0130a3af496bc9f7779
Danny Cardenas <Danny.Cardenas@bayteq.com>
2022-09-11 14:44:09 -0500

    Change Images

--- js/login.js
+++ js/login.js
@@ -2,6 +2,6 @@ function start() {
   var username = document.getElementById("username").value;
   var password = document.getElementById("password").value;
   if (username == "July" && password == "123") {
-    window.location.href = "/content.html";
+    window.location.href = "content.html";
   }
 }
