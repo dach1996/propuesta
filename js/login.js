329d38d63aeb2807a8d72aa3ddfd5906bf684b57
Danny Cárdenas <danny.cardenas@bayteq.com>
2023-07-28 21:58:50 -0500

    Change 2023

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 function start() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-  if (username == "July" && password == "28Jul2022") {
+  if (username == "July" && password == "28Jul2023") {
     window.location.href = "content.html";
   }
 }
