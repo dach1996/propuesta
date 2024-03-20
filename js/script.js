956a76a8847af2c3720501f76028104d3c5c8034
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-03-20 00:23:42 -0500

    Change Text

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 document.getElementById("text").innerHTML =
-  "Hola July, espero que te encuentres bien, ¿sabes qué día es hoy?";
-document.getElementById("nextButtom").innerHTML = "No, ¿que día es hoy?";
+  "Hola July, espero que te encuentres bien";
+document.getElementById("nextButtom").innerHTML = "Sí, dentro de lo que cabe";
+window.addEventListener('load', function () {
+  const audio = document.querySelector("audio");
+  audio.volume = 0.8;
+  audio.play();
+});
 
 let messageCount = 0;
 function myFunction() {
@@ -30,48 +35,65 @@ function myFunction() {
     switch (messageCount) {
       case 1:
         messageSeleted =
-          "Hoy es 21 de marzo, un día normal para unas personas y muy especial para ciertas chicas, ¿Sabes por qué?";
-        messageBottonSelected = "No";
-        const audio = document.querySelector("audio");
-        audio.volume = 0.8;
-        audio.play();
+          "Me alegra mucho, ¿sabes qué día es hoy?";
+        messageBottonSelected = "mmm, Nop";
         break;
       case 2:
+        messageSeleted =
+          "Hoy es 21 de marzo, un día normal para unas personas y muy especial para ciertas chicas, ¿Sabes por qué?";
+        messageBottonSelected = "No";
+        break;
+      case 3:
         messageSeleted =
           "El día de hoy marca el inicio de la primavera y es cuando las flores más bonitas hacen su aparición.";
         messageBottonSelected = "¿Enserio?";
         break;
-      case 3:
+      case 4:
         messageSeleted =
           "Perdón por volver a escribir luego de lo que pasó, pero recordé la bonita sonrisa que te causaba esta fecha.";
         messageBottonSelected = "Tranquilo";
         break;
-      case 4:
-        messageSeleted =
-          "Aunque no lo creas la sensación más bonita para un chico es hacer feliz a la chica que le gusta";
-        messageBottonSelected = "No lo sabía";
-        break;
       case 5:
         messageSeleted =
-          "No sé si el día de hoy recibiste o no las flores amarillas, pero en caso de no haberlas recibido no te preocupes.";
-        messageBottonSelected = "¿Por qué?";
+          "En fin, No sé si el día de hoy recibiste o no tus flores amarillas.";
+        messageBottonSelected = "Quizá, ¿Por qué?";
         break;
       case 6:
+        messageSeleted =
+          "Bueno en caso de no haberlas recibido no te preocupes.";
+        messageBottonSelected = "¿Por qué?";
+        break;
+      case 7:
         messageSeleted =
           "Porque tengo una pequeña sorpresa para ti";
         messageBottonSelected = "¿Cuál es?";
         break;
-      case 7:
+      case 8:
         messageSeleted =
           "Sé lo importante que es esta fecha para ti y la sensación tan bonita que sientes al recibir tus florecitas amarillas.";
         messageBottonSelected = "Sí, me gustan mucho";
         break;
-      case 8:
-        messageSeleted =
-          "Si quieres tu sopresa debes ir a reclamar tu sorpresa en la garita con el guardia.";
-        messageBottonSelected = "Ok voy...";
-        break;
       case 9:
+        messageSeleted =
+          "Quizá no es algo tan grande ni tan costoso materialmente, pero me pareció algo lindo.";
+        messageBottonSelected = "¿Me indicas?";
+        break;
+      case 10:
+        messageSeleted =
+          "Tranquila, ¿En serio quieres tu sorpresa?";
+        messageBottonSelected = "Sí...";
+        break;
+      case 11:
+        messageSeleted =
+          "Tendrás que caminar un poco, tranquila";
+        messageBottonSelected = "¿A qué te refieres?";
+        break;
+      case 12:
+        messageSeleted =
+          "Deberás digirte a la garita, el guardia tendrá tu sorpresa.";
+        messageBottonSelected = "mmm... Ok iré a ver...";
+        break;
+      case 13:
         messageSeleted =
           "Espero poderte haber alegrado el día";
         messageBottonSelected = "Gracias";
